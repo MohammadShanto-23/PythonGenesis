6939119f372ee6c95c1dd53682fac9ced7846624
Mohammad Shanto <mohammadjhshanto01@gmail.com>
2025-10-28 22:33:37 +0600

    Added type casting demo code

--- Type Casting.py
+++ Type Casting.py
@@ -1,1 +1,19 @@
+# Type casting means converting one data type to another
+
+a = 10         # int
+b = 3.5        # float
+c = "25"       # string
+
+# Convert int → float
+a_to_float = float(a)
+
+# Convert float → int
+b_to_int = int(b)
+
+# Convert string → int
+c_to_int = int(c)
+
+print(a_to_float, type(a_to_float))
+print(b_to_int, type(b_to_int))
+print(c_to_int, type(c_to_int))
 
