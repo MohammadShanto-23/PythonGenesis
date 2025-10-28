d51362958cb0bb42da3cecdf68c0fd59a98d2b4c
Mohammad Shanto <mohammadjhshanto01@gmail.com>
2025-10-28 22:21:50 +0600

    Updated

--- Variables and Data Types.py
+++ Variables and Data Types.py
@@ -1,1 +1,32 @@
+# Variables in Python
 
+# Integer variable
+age = 22
+
+# Float variable
+height = 5.9
+
+# String variable
+name = "Mohammad Shanto"
+
+# Boolean variable
+is_student = True
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print(sep=" ")
+# Different data types in Python
+
+integer_num = 10           # int
+float_num = 3.14           # float
+string_text = "Hello Python!"  # str
+boolean_val = False        # bool
+
+print(type(integer_num))
+print(type(float_num))
+print(type(string_text))
+print(type(boolean_val))
